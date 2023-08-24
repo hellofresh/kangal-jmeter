ea2eaf707c58d349757d25991ba8b59ec94a921a
Jonas Mardosas <jonas@omnisend.com>
2023-08-24 13:54:56 +0200

    add custom report uploader

--- docker/jmeter-master/launcher.sh
+++ docker/jmeter-master/launcher.sh
@@ -19,8 +19,9 @@ run_jmeter_test() {
       cp results.csv /results/results.csv
       if [[ -n "${REPORT_PRESIGNED_URL}" ]]; then
         echo "=== Saving report to Object storage ==="
-        tar -C /results -cf results.tar .
-        curl -X PUT -H "Content-Type: application/x-tar" -T results.tar -L "${REPORT_PRESIGNED_URL}"
+        tar -C /results -cf results-${WORKER_SVC_NAME}.tar .
+        # curl -X PUT -H "Content-Type: application/x-tar" -T results.tar -L "${REPORT_PRESIGNED_URL}"
+        curl --form file='@results.tar' "gcs-uploader.kangal.svc.cluster.local/upload"
       elif [[ -n "${AWS_BUCKET_NAME}" ]]; then
         echo "WARNING: Using AWS credentials to upload reports is deprecated. Kangal upgrade is recommended."
         echo "=== Trying to send report to ${AWS_BUCKET_NAME}/${LOADTEST_NAME} endpoint ${AWS_ENDPOINT_URL} ==="
